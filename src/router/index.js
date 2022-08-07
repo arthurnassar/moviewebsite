6b2c31c6d27758b75595ddb27b38d937f4416f23
arthurnassar <arthurnassar@gmail.com>
2022-08-07 19:27:47 +0100

    fix(bug): Pagina de status com redirecionamento errado

--- src/router/index.js
+++ src/router/index.js
@@ -36,7 +36,7 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/ANYYY.vue'),
+    component: () => import(/* webpackChunkName: "about" */ '../views/StatusPage.vue'),
     meta: {
       needsAuth: true
     }
